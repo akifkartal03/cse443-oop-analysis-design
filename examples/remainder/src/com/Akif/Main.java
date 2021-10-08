edce36623ca22f3c1918918f7c039f5b14ba9bb2
akifkartal03 <akokartal@gmail.com>
2021-10-08 18:39:42 +0300

    finish project

--- examples/remainder/src/com/Akif/Main.java
+++ examples/remainder/src/com/Akif/Main.java
@@ -7,24 +7,15 @@ import java.util.concurrent.locks.ReentrantLock;
 public class Main {
 
     public static void main(String[] args) {
-	    /*King player1 = new King();
-        player1.setWeapon(new Knife());
-        Queen player2 = new Queen();
-        player2.setWeapon(new BowAndArrow());
-        Troll player3 = new Troll();
-        player3.setWeapon(new Axe());
-        player1.fight();
-        player2.fight();
-        player3.fight();
-        Integer a = 5;
-        a++;
-        System.out.println(a);*/
+	    /*Create common variables*/
         ReentrantLock mutex = new ReentrantLock();
         Condition empty= mutex.newCondition();
         Condition full = mutex.newCondition();
         int capacity = 10;
         AtomicInteger buffer = new AtomicInteger(capacity);
         AtomicInteger power = new AtomicInteger(100);
+
+        /*Create Threads*/
         ThreadData commonData = new ThreadData(buffer,mutex,full,empty,capacity,power);
         Thread producerOttomanWoman = new Thread(new OttomanWoman(commonData));
         producerOttomanWoman.setName("Ottoman Woman Fatma");
@@ -37,8 +28,7 @@ public class Main {
         Thread consumerQueen = new Thread(player2);
         consumerQueen.setName("Queen");
 
-
-
+        /*Start Threads*/
         producerOttomanWoman.start();
         consumerKing.start();
         consumerQueen.start();
